d4bb3a7a669ad09b1e1e0e934c4e069a0c4a77b1
José Ángel Cortés Montoya <jose.cortes@onoff.com.co>
2024-09-20 16:49:18 -0500

    seccion 6, clase 80

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'MsSatan',
+      power: 1,
+    },
+  ];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+
+    this.onDelete.emit(id);
+  }
+}
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+console.log(uuid());
+
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({ providedIn: 'root' })
+export class DbzService {
+  public characters: Character[] = [
+    { id: uuid(), name: 'Goku', power: 19000 },
+    { id: uuid(), name: 'Vegeta', power: 18900 },
+    { id: uuid(), name: 'Trunks', power: 11000 },
+  ];
+
+  addCharacter(character: Character): void {
+    const newCharacter: Character = {
+      id: uuid(),
+      ...character,
+    };
+    this.characters.push(newCharacter);
+  }
+
+  // onDeleteCharacter(index: number) {
+  //   this.characters.splice(index, 1);
+  // }
+  deleteCharacterById(id: string) {
+    this.characters = this.characters.filter(
+      (character) => character.id !== id
+    );
+  }
+}
